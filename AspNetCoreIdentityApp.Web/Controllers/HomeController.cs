c3a1cf62f8397209231fe4e19021f557e1408139
Emre <emre.coskun@bs.nttdata.com>
2023-05-03 16:29:12 +0200

    Add project files.

---
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModal;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreIdentityApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
+
+
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel parameters)
+        {
+            var identityResult = await _userManager.CreateAsync(new() { UserName = parameters.UserName, PhoneNumber = parameters.PhoneNumber, Email = parameters.Email }, parameters.Password);
+            if (identityResult.Succeeded)
+            {
+                ViewBag.Message = "Sign in process is successful.";
+                return View();
+            }
+            foreach (IdentityError item in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, item.Description);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCoreIdentityApp.Web/ViewModal/SignUpViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModal
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel()
+        {
+            
+        }
+        public SignUpViewModel(string userName, string email, string phoneNumber, string password, string passwordConfirm)
+        {
+            UserName = userName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+            PasswordConfirm = passwordConfirm;
+        }
+        [Display(Name = "User Name :")]
+        public string UserName { get; set; }
+        [Display(Name = "E-Mail :")]
+        public string Email { get; set; }
+        [Display(Name = "Phone Number :")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Password :")]
+        public string Password { get; set; }
+        [Display(Name = "Password Confirm :")]
+        public string PasswordConfirm { get; set; }
+    }
+}
